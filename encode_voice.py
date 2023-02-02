6c371afe27d0123ce06d22db74d461db952c7e6e
AFinlayV <afinlayv@gmail.com>
2023-02-02 16:05:23 -0500

    Init commit of a script that generates a podcast from a list of topics

---
+++ encode_voice.py
@@ -0,0 +1,5 @@
+"""
+This script will take in a voice file as wav or mp3, and build a model based on the voice.
+It will use the ElevenLabs API to build the model based on an example voice file.
+I will build this once I have the main script working
+"""
